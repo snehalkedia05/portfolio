b11bb715f3a625dbf2e7d7935ef1fd69df008859
snehalkedia05 <114679537+snehalkedia05@users.noreply.github.com>
2023-04-12 19:22:42 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* typing animation*/
+var typed  = new Typed(".typing",{
+    strings:["","IT Engineer","Web Developer","UI/UX Designer","Problem Solver"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
